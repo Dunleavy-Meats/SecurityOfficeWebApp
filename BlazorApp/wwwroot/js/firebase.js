d7d56e2ec09d9eaf06fbbb976991671dc675589c
Mohamed Shiha <meatsdunleavy@gmail.com>
2025-05-24 17:40:34 +0100

    fixed issues with api key

--- BlazorApp/wwwroot/js/firebase.js
+++ BlazorApp/wwwroot/js/firebase.js
@@ -1,16 +1,6 @@
 ﻿
-const firebaseConfig = {
-    apiKey: "AIzaSyCg2RGzOWlGY55WcMktt7Vg1l5r30ece4Y",
-    authDomain: "security-system-c0b0e.firebaseapp.com",
-    projectId: "security-system-c0b0e",
-    storageBucket: "security-system-c0b0e.firebasestorage.app",
-    messagingSenderId: "565564609130",
-    appId: "1:565564609130:web:fce3021f128b764e3d7013",
-    measurementId: "G-4SQ8R3FM4M"
-};
-
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(window.firebaseConfig);
 
 
 window.firebaseSignInWithEmailAndPassword = async (email, password) => {
