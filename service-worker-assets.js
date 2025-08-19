a1286fdcaac2657f5a624e3b895e4434e5aabf10
Dunleavy-Meats <meatsdunleavy@gmail.com>
2025-08-19 21:50:59 +0000

    Deploying to gh-pages from @ Dunleavy-Meats/SecurityOfficeWebApp@c848289807dbe45f3e9cecf653370576c9796e58 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -72,11 +72,11 @@
       "url": "_framework\/icudt.dat"
     },
     {
-      "hash": "sha256-sQ12Mb7w5V8F0RkbLo8e13LD8l\/EpTULEjs9OJHerlk=",
+      "hash": "sha256-X9PKXusl+lJp7TyKUgcNPLvP5Hh18k85BjSGOVG0r8s=",
       "url": "_framework\/blazor.boot.json"
     },
     {
-      "hash": "sha256-EX\/q5t\/J7ZuirTConKnc9r28WLy64v6AFHRRp1YqhQM=",
+      "hash": "sha256-7kdxkuRpeNMzu4BRIgaQenJnaJ34Ha3RW\/eVMD0WRhA=",
       "url": "_framework\/BlazorApp.dll"
     },
     {
@@ -496,5 +496,5 @@
       "url": "_content\/MudBlazor\/MudBlazor.min.js"
     }
   ],
-  "version": "dgGzmYMS"
+  "version": "7eMgRnZY"
 };
--- service-worker.js
+++ service-worker.js
@@ -36,4 +36,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: dgGzmYMS */
+/* Manifest version: 7eMgRnZY */
