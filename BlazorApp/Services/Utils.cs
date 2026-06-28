feed4cbfa17710cb16cf427aa71b07d5b4dcde13
Dunleavy-Meats <meatsdunleavy@gmail.com>
2026-06-28 19:36:33 +0100

    added a format method to all dates

---
+++ BlazorApp/Services/Utils.cs
@@ -0,0 +1,22 @@
+﻿using Models;
+
+namespace BlazorApp.Services
+{
+    public static class Utils
+    {
+        public static string GetFormatedTimeAndDate(DateTime date)
+        {
+            return date.ToString(@"dd\/MM\/yyyy HH:mm tt");
+        }
+
+        public static string GetLastVisit(Visitor visitor)
+        {
+
+            if (visitor.LastVisit == null)
+                return "No previous visits";
+
+            var lastVisitDate = visitor.LastVisit.Value.ToLocalTime();
+            return GetFormatedTimeAndDate(lastVisitDate);
+        }
+    }
+}
